d2590c2ad8b6dec716ef84a7c4b1a61bcf44f30f
umapena <maria.alicemp@hotmail.com>
2021-12-06 23:01:33 -0300

    .

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LivrosComponent } from './livros/livros.component';
 import { HomeComponent } from './home/home.component';
+import { LivrosModule } from './livros/livros.module';
+import { LivrosService } from './livros.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LivrosComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    LivrosModule
   ],
-  providers: [],
+  providers: [
+    LivrosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
